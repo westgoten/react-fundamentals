5af259a1848caa1de189b6bcffe6e45139b02f44
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2022-07-25 23:24:00 -0300

    feat: finish exercise 06 extra 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,23 +3,45 @@
 
 import * as React from 'react'
 
+const ERROR_MESSAGE = 'Username must be lower case'
+
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef()
+  const [error, setError] = React.useState(null)
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="form" onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input ref={inputRef} id="username" type="text" />
+        <input
+          ref={inputRef}
+          id="username"
+          type="text"
+          onChange={handleChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button className="submitButton" type="submit" disabled={!!error}>
+        Submit
+      </button>
+      {error && <p className="errorMessage">{error}</p>}
     </form>
   )
 
   function handleSubmit(event) {
     event.preventDefault()
-    const username = inputRef.current?.value ?? ''
-    onSubmitUsername(username)
+    if (!error) {
+      const username = inputRef.current?.value ?? ''
+      onSubmitUsername(username)
+    }
+  }
+
+  function handleChange(event) {
+    const username = event.target.value
+    if (username === username?.toLowerCase()) {
+      setError(null)
+    } else {
+      setError(ERROR_MESSAGE)
+    }
   }
 }
 
